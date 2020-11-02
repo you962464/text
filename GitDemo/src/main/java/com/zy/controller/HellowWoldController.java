9c597b1d4e11d36d91346cf8c5dd878e74b2eba3
you962464 <63383305+you962464@users.noreply.github.com>
2020-11-02 14:41:55 +0800

    Update HellowWoldController.java

--- GitDemo/src/main/java/com/zy/controller/HellowWoldController.java
+++ GitDemo/src/main/java/com/zy/controller/HellowWoldController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HellowWoldController {
     @RequestMapping(value = "/hellow")
     public String hellow(){
+        
+         String a = "123";
+        
         System.out.println(" lalalla " );
+        
         return "HellowWold";
     }
 }
